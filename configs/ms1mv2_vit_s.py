ff7cfe13ddd51c705d6fab04bd3b69e693449241
Dv04 <62090715+Dv04@users.noreply.github.com>
2024-12-09 15:47:40 +0530

    Add .gitignore, update requirements, and modify config paths

--- configs/ms1mv2_vit_s.py
+++ configs/ms1mv2_vit_s.py
@@ -4,7 +4,7 @@ from easydict import EasyDict as edict
 # our RAM is 256G
 # mount -t tmpfs -o size=140G  tmpfs /train_tmp
 config = edict()
-config.margin_list = (1.0, 0.5, 0.0)   # arcface
+config.margin_list = (1.0, 0.5, 0.0)  # arcface
 config.network = "vit_s_dp005_mask_0"
 config.resume = False
 config.output = None
@@ -18,7 +18,7 @@ config.lr = 0.001
 config.verbose = 2000
 config.dali = False
 
-config.rec = "/mnt/workspace/faces_emore"  #MS1MV2
+config.rec = "faces_emore"  # MS1MV2
 config.num_classes = 85742
 config.num_image = 5822653
 config.num_epoch = 35
